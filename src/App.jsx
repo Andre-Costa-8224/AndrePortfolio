b77af32ced3f7fa7e51c3b04ea91b8974a19a115
andrecosta <andrecosta8224@gmail.com>
2022-03-03 12:19:15 -0300

    atualização

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bgmenu">
+        <div class="container menu">
+          <a class="navbar-brand" href="#">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C4E03AQEr68Xe2nvp3w/profile-displayphoto-shrink_800_800/0/1637932804494?e=1651708800&v=beta&t=CwH80PweFTcR9zef6Imj4WktaOXaGWriV5Obl39V2eg"
+              alt=""
+              className="imgborder"
+              width="88"
+              height="88"
+            />
+          </a>
+          <a href="" className="linkstyle">projetos</a>
+          <a href="" className="linkstyle">contato</a>
+          <a href="" className="linkstyle">sobre mim</a>
+        </div>
+      </nav>
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
